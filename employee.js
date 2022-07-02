7c9de2ae08c78143fe26e1dca33af2b34a2e59eb
puja <pujarana1796@gmail.com>
2022-07-02 11:14:52 +0530

    using template literal

---
+++ employee.js
@@ -0,0 +1,30 @@
+window.addEventListener('DOMContentLoaded', (Event) =>{
+    createInnerHtml();
+});
+
+const createInnerHtml = () => {
+    const headerHtml = "<th>Profile</th><th><pre >Name</pre></th><th>Gender</th><th>Department</th><th>Salary</th><th>startDate</th><th>Action</th>";              
+                                        
+    const innerHtml = `${headerHtml}
+                       
+                <tr>
+                    <td><img class="profile" alt="" src="image/profile2.jpg"></td>
+                    <td id = "vv" >Rahul Rana </td>
+                    <td>Male</td>
+                    <td><div class='dept-label'>Sales</div>
+                        <div class='dept-label'>HR</div>
+                        <div class='dept-label'>Finance</div></td>
+                 
+                    <td>300000</td>
+                    <td>1 Nov 2020</td>
+                    <td>
+                        <img id="1" class = "Icon delete" onclick="remove(this)" alt="delete"
+                            src="image/deleteicon.jpg">
+                        <img id="1" class = "Icon" alt="edit" onclick="update(this)" alt="edit"
+                            src="image/update.png">
+                    </td>         
+                </tr>
+                
+    `;
+    document.querySelector('#table-display').innerHTML = innerHtml;
+}
